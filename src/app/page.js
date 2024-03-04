cb78d01cd18c68530dc054a8d84cac16de14db34
Tin Xian <tinxian.hu@hotmail.com>
2024-03-04 11:31:18 +0100

    Initial Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,268 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Link from "next/link";
+import { API_KEY, API_URL } from "./constants";
 
 export default function Home() {
+  const [domainName, setDomainName] = useState("");
+  const [domainData, setDomainData] = useState(null);
+  const [cartItems, setCartItems] = useState([]);
+  const [isCartVisible, setIsCartVisible] = useState(false);
+
+  const tlds = [
+    "com",
+    "nl",
+    "org",
+    "net",
+    "io",
+    "ai",
+    "app",
+    "dev",
+    "tech",
+    "co",
+    "io",
+    "design",
+    "blog",
+    "shop",
+    "store",
+    "online",
+    "website",
+    "site",
+    "host",
+    "hosting",
+    "cloud",
+  ];
+
+  const useLocalData = true;
+
+  // Load cart items from localStorage on component mount
+  useEffect(() => {
+    const storedCartItems = localStorage.getItem("cartItems");
+    if (storedCartItems) {
+      setCartItems(JSON.parse(storedCartItems));
+    }
+  }, []);
+
+  // Function to toggle cart visibility
+  const toggleCartVisibility = () => {
+    setIsCartVisible(!isCartVisible);
+  };
+
+  // Function to check domain availability
+  const handleCheckDomain = async () => {
+    if (!domainName) {
+      alert("Please enter a domain name");
+      return;
+    } else if (domainName.length < 3) {
+      alert("Please enter a valid domain name");
+      return;
+    }
+
+    if (useLocalData) {
+      const localDataArray = [];
+
+      for (let i = 0; i < tlds.length; i++) {
+        const localData = {
+          domain: domainName,
+          price: Math.floor(Math.random() * 99 + 1).toFixed(2),
+          status: Math.random() > 0.5 ? "free" : "active",
+          tld: tlds[i],
+        };
+        localDataArray.push(localData);
+      }
+      setDomainData(localDataArray);
+    } else {
+      try {
+        const response = await axios.post(
+          `${API_URL}`,
+          {
+            name: domainName,
+            extension: "nl",
+          },
+          {
+            headers: {
+              Authorization: `Basic ${API_KEY}`,
+              "Content-Type": "application/json",
+              "Cache-Control": "no-cache",
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          },
+        );
+
+        if (response.status === 200) {
+          console.log("Domain information:", response.data);
+          setDomainData([response.data]);
+        } else {
+          console.error("Failed to fetch domain information");
+          console.error(response);
+        }
+      } catch (error) {
+        console.error("Error during API request:", error);
+      }
+    }
+  };
+
+  // Function to add domain to cart
+  const addToCart = (domain) => {
+    if (
+      cartItems.some(
+        (item) => item.domain === domain.domain && item.tld === domain.tld,
+      )
+    ) {
+      alert("This domain is already in the cart.");
+      return;
+    }
+
+    const updatedCart = [...cartItems, domain];
+    setCartItems(updatedCart);
+
+    // Update localStorage
+    localStorage.setItem("cartItems", JSON.stringify(updatedCart));
+  };
+
+  // Function to remove domain from cart
+  const removeFromCart = (index) => {
+    const updatedCart = [...cartItems];
+    updatedCart.splice(index, 1);
+    setCartItems(updatedCart);
+
+    // Update localStorage
+    localStorage.setItem("cartItems", JSON.stringify(updatedCart));
+  };
+
+  // Function to calculate subtotal
+  const Cart = () => {
+    const calculateSubtotal = () => {
+      return cartItems.reduce(
+        (total, item) => total + parseFloat(item.price),
+        0,
+      );
+    };
+
+    return (
+      <div className="cart-popup absolute top-full mt-2 p-4 bg-white border rounded-lg shadow-md text-black">
+        <h3 className="mb-4 text-lg font-semibold">Your Cart</h3>
+        {cartItems.map((item, index) => (
+          <div key={index} className="mb-4 flex border-b pb-2 last:border-b-0">
+            <div className="ml-3">
+              <p className="text-sm font-medium">
+                {item.domain}.{item.tld}
+              </p>
+              <p className="text-gray-600 text-sm">Price: €{item.price}</p>
+            </div>
+            <button
+              className="cart-delete align-self-end ml-5 text-red-500 hover:text-red-700"
+              onClick={() => removeFromCart(index)}
+            >
+              Remove
+            </button>
+          </div>
+        ))}
+        {cartItems.length === 0 && (
+          <p className="mt-4 text-gray-500">Your cart is empty.</p>
+        )}
+        {cartItems.length > 0 && (
+          <>
+            <h5 className="mt-4">
+              Subtotal: €{calculateSubtotal().toFixed(2)}
+            </h5>
+            <div className="mt-4 flex justify-end">
+              <Link href={{ pathname: "/cart" }}>
+                <button className="bg-blue-500 text-white py-2 px-4 rounded-md">
+                  Checkout
+                </button>
+              </Link>
+            </div>
+          </>
+        )}
+      </div>
+    );
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <main>
+      <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700 sticky top-0">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="#"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Domain Checker
+            </span>
           </a>
+          <div
+            className="hidden w-full md:block md:w-auto"
+            id="navbar-solid-bg"
+          >
+            <div className="cart-icon" onClick={toggleCartVisibility}>
+              🛒
+            </div>
+          </div>
         </div>
-      </div>
+        {isCartVisible && <Cart />}
+      </nav>
+      <div className="flex flex-col pt-24 px-10 items-center">
+        <div className="max-w-xl w-full items-center justify-between font-mono text-sm flex mb-5">
+          <input
+            type="text"
+            placeholder="Type here your domain name"
+            className="w-full p-2 rounded-s-lg text-black"
+            value={domainName}
+            onChange={(e) => setDomainName(e.target.value)}
+          />
+          <button
+            className="p-2 bg-blue-500 text-white rounded-r-lg"
+            onClick={handleCheckDomain}
+          >
+            Check
+          </button>
+        </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        {domainData &&
+          domainData.map((data, dataIndex) => (
+            <div
+              key={dataIndex}
+              className="max-w-xl w-full mx-auto mb-4 bg-white rounded-lg shadow-md overflow-hidden"
+            >
+              <div className="p-4 flex items-center justify-between">
+                <div className="flex-row items-start">
+                  <p className="text-lg text-black">
+                    <span>{data.domain}</span>
+                    <span className="font-bold">.{data.tld}</span>
+                  </p>
+                  <span
+                    className={` p-1 rounded-lg text-sm ${
+                      data.status === "free"
+                        ? "text-green-700 bg-green-100"
+                        : "text-red-700 bg-red-100"
+                    }`}
+                  >
+                    {data.status === "free" ? "Available" : "Not available"}
+                  </span>
+                </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+                <div className="flex items-center justify-items-start">
+                  <p className="mr-4 text-lg text-black font-bold">
+                    €{data.price}
+                  </p>
+                  {data.status === "free" ? (
+                    <button
+                      className="atc-button bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+                      onClick={() => addToCart(data)}
+                    >
+                      Add to cart
+                    </button>
+                  ) : (
+                    <button className="atc-button bg-red-500 text-white rounded-lg cursor-not-allowed">
+                      Not available
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          ))}
       </div>
     </main>
   );
